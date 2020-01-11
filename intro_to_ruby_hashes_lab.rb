9c13209a6e40b07f8c537c55787d1957b2d5733f
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-11 22:32:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,5 +57,5 @@ def monopoly_with_fourth_tier
 	    }
 	  }
 	}
-  [:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  [:railroads][:names][:reading_railroad][:"mortgage_value"] = "$100"
 end
